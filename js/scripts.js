55e8ecebf1fd8f9fdc554aa631f94ce59d4896b3
lorenz2396 <lorenzo.favaretto1996@gmail.com>
2023-06-29 12:07:16 +0200

    final exercise done

--- js/scripts.js
+++ js/scripts.js
@@ -56,3 +56,71 @@ const posts = [
     }
 ];
 
+const postsContainer = document.getElementById('container');
+
+const likedPost = [];
+
+posts.forEach(singlePost => {
+
+    postsContainer.innerHTML +=
+    `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${singlePost.author.image}" alt="${singlePost.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${singlePost.author.name}</div>
+                        <div class="post-meta__time">${singlePost.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src="${singlePost.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${singlePost.id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${singlePost.id}" class="js-likes-counter">${singlePost.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `;
+});
+
+const likedButton = document.querySelectorAll('.js-like-button');
+
+likedButton.forEach((singleLikedButton, i) => {
+    singleLikedButton.addEventListener('click',
+        function(evento){               //evento contiene l'elemento click
+            evento.preventDefault();
+
+            if(!this.classList.contains('like-button--liked')){              
+                this.classList.add('like-button--liked');
+
+                const postId = parseInt(this.getAttribute('data-postid'));
+                // likedPost.push(posts[i].id);
+                likedPost.push(postId);
+
+                const likeCounter = document.getElementById(`like-counter-${this.getAttribute('data-postid')}`)
+                console.log(likeCounter.innerText);
+
+                let currentLikeNumber = parseInt(likeCounter.innerHTML);
+
+                likeCounter.innerHTML = currentLikeNumber + 1;
+                // likeCounter.innerHTML = ++currentLikeNumber;
+            }
+
+            console.log(likedPost);
+        }
+    );
+});
